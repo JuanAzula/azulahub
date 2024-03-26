9a9168f1eb434a4b4b6651aaf30da893f226fd41
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-03-26 12:55:34 +0100

    Add jest

---
+++ back/src/tests/movies.spec.ts
@@ -0,0 +1,15 @@
+// import { getMovies } from "../controllers/movies.controller";
+import app from "../server";
+import request from "supertest";
+
+describe('movies petitions', () => {
+    test('getMovies', async () => {
+        const mockRequest = {
+            params: {}, // You can add any required properties here
+            query: {}, // You can add query parameters here
+            body: {}, // You can add a request body if needed
+        };
+        const movies = await request(app).get('/api/movies').send(mockRequest)
+        expect(movies.status).toBe(200)
+    })
+})
